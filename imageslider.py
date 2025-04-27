4e83ed6854b938f1aa4d29efdc45fcc4e728904b
Yuji Arakawa <yuji.magic.arakawa@gmail.com>
2025-04-27 16:21:44 +0900

    画像パスを変更しました。

--- imageslider.py
+++ imageslider.py
@@ -1,20 +1,18 @@
 import gradio as gr
-from PIL import Image
 
 def create_imageslider():
     # 画像のパスを設定
     image_paths = (
-        "images/palepink.png",
-        "images/black_and_white.png"
+        "palepink.png",
+        "black_and_white.png"
     )
     
     # Gradioアプリの作成
     with gr.Blocks() as demo:
         gr.ImageSlider(
             label="イメージスライダーデモ",
-            value=image_paths,  # 画像パスのタプルを渡す
             show_label=True,
-            container=True
+            value=image_paths,
         )
     
     return demo
@@ -22,6 +20,6 @@ def create_imageslider():
 if __name__ == "__main__":
     demo = create_imageslider()
     demo.launch(
-        inbrowser=True,  # ブラウザを自動的に開く
-        show_error=True  # エラーを表示
+        inbrowser=True,
+        show_error=True
     )
